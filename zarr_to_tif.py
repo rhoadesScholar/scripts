7343026e6b17ed092a89925485a8e55a6951a534
rhoadesScholar <rhoadesj@hhmi.org>
2023-12-05 10:03:39 -0500

    feat: ✨ commit rhoadesj scripts

--- zarr_to_tif.py
+++ zarr_to_tif.py
@@ -3,6 +3,7 @@ from funlib.persistence import open_ds
 import numpy as np
 from tifffile import imwrite
 import os
+
 # import click
 
 
@@ -17,6 +18,7 @@ import os
 # def cli(log_level):
 #     logging.basicConfig(level=getattr(logging, log_level.upper()))
 
+
 # @cli.command()
 # @click.argument('zarr_path', type=click.Path(exists=True))
 # @click.argument('zarr_dataset', type=str)
@@ -24,29 +26,35 @@ import os
 # @click.option('--invert', is_flag=True)
 def zarr_to_tif(zarr_path, zarr_dataset, tif_path, invert=False):
     print(f"Loading {zarr_path}:{zarr_dataset} ...")
-    dataset = open_ds(zarr_path, zarr_dataset, mode='r')
+    dataset = open_ds(zarr_path, zarr_dataset, mode="r")
     img = dataset.to_ndarray()
 
     if invert:
         print("Inverting ...")
         if dataset.dtype == np.uint8:
             img = 255 - img
-        else: # assume float
+        else:  # assume float
             img = 1 - img
 
     print(f"Saving {tif_path} ...")
     os.makedirs(os.path.dirname(tif_path), exist_ok=True)
     imwrite(tif_path, img)
 
+
 # dataset = "jrc_mus-liver-zon-2"
-dataset = "jrc_mus-liver"
+# dataset = "jrc_mus-liver"
+dataset = "jrc_mus-liver-3"
 level = "s5"
 invert = True
 
 zarr_path = "/nrs/cellmap/data/{dataset}/{dataset}.n5".format(dataset=dataset)
-# zarr_dataset = "em/fibsem-uint8/{level}".format(level=level)
-zarr_dataset = "volumes/raw/{level}".format(level=level)
-tif_path = "/nrs/cellmap/rhoadesj/tmp_data/tiffs/{dataset}/{dataset}_{level}.tif".format(dataset=dataset, level=level)
+zarr_dataset = "em/fibsem-uint8/{level}".format(level=level)
+# zarr_dataset = "volumes/raw/{level}".format(level=level)
+tif_path = (
+    "/nrs/cellmap/rhoadesj/tmp_data/tiffs/{dataset}/{dataset}_{level}.tif".format(
+        dataset=dataset, level=level
+    )
+)
 
 zarr_to_tif(zarr_path, zarr_dataset, tif_path, invert=invert)
 
